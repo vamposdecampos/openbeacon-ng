bf4a4065008a20ab20cb985fef305f5a86e060c5
Alex Badea <vamposdecampos@gmail.com>
2020-09-10 21:58:13 -0700

    firmware: unify timer module
    The timer.{c,h} sources are now identical across projects (give or take the
    timer_wait_ms macro).  Move them under core/openbeacon/ and tweak OPTION_SRC
    in Makefiles accordingly.

--- firmware/nRF51/core/openbeacon/inc/timer.h
+++ firmware/nRF51/core/openbeacon/inc/timer.h
@@ -2,7 +2,7 @@
  *
  * OpenBeacon.org - nRF51 Timer Routines
  *
- * Copyright 2013 Milosch Meriac <meriac@openbeacon.de>
+ * Copyright 2013-2015 Milosch Meriac <meriac@openbeacon.de>
  *
  ***************************************************************
 
@@ -29,6 +29,7 @@
 
 #define SECONDS(x) ((uint32_t)((LF_FREQUENCY*x)+0.5))
 #define MILLISECONDS(x) ((uint32_t)(((LF_FREQUENCY*x)/1000.0)+0.5))
+#define timer_wait_ms(x) timer_wait(MILLISECONDS(x))
 
 extern void timer_init(void);
 extern void timer_wait(uint32_t ticks);
