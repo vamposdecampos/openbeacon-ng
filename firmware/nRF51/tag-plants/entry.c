c7dfca562def5603eb3f465098b9acfd87d695cc
Alex Badea <vamposdecampos@gmail.com>
2020-09-16 18:49:52 -0700

    tag-plants: run ADC sequencing from main loop
    Also invert LED GPIO polarity, since we'll be driving a P-MOSFET
    with the same GPIO.

--- firmware/nRF51/tag-plants/entry.c
+++ firmware/nRF51/tag-plants/entry.c
@@ -2,6 +2,8 @@
 #include <adc.h>
 #include <temp.h>
 
+static BOOL g_debug = false;
+
 /* physical web beacon packet */
 static uint8_t g_beacon_pkt[] = {
 #if 0
@@ -24,18 +26,14 @@ static uint8_t g_beacon_pkt[] = {
 
 void radio_start_hook(void)
 {
-	pin_set(CONFIG_LED_PIN);
 }
 
 void radio_hfclk_hook(void)
 {
-	temp_start();
-	adc_start();
 }
 
 void adc_done_hook(void)
 {
-	pin_clear(CONFIG_LED_PIN);
 }
 
 void radio_advertise_hook(void)
@@ -47,6 +45,18 @@ void radio_advertise_hook(void)
 	g_beacon_pkt[sizeof(g_beacon_pkt)-3] = (temp >> 8) & 0xff;
 	g_beacon_pkt[sizeof(g_beacon_pkt)-2] = adc_bat();
 	g_beacon_pkt[sizeof(g_beacon_pkt)-1] = adc_ain();
+	if (g_debug)
+		debug_printf("ain %d\r\n", adc_ain());
+}
+
+static inline void led_on(void)
+{
+	pin_clear(CONFIG_LED_PIN);
+}
+
+static inline void led_off(void)
+{
+	pin_set(CONFIG_LED_PIN);
 }
 
 void entry(void)
@@ -55,11 +65,13 @@ void entry(void)
 	nrf_gpio_cfg_input(CONFIG_TEST_PIN, GPIO_PIN_CNF_PULL_Pulldown);
 
 	/* drive strength: standard 0, high 1 */
+	led_off();
 	nrf_gpio_cfg_output(CONFIG_LED_PIN);
 	NRF_GPIO->PIN_CNF[CONFIG_LED_PIN] =
 		(GPIO_PIN_CNF_DIR_Output       << GPIO_PIN_CNF_DIR_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos) |
-		(GPIO_PIN_CNF_DRIVE_S0H1       << GPIO_PIN_CNF_DRIVE_Pos);
+		(GPIO_PIN_CNF_DRIVE_S0S1       << GPIO_PIN_CNF_DRIVE_Pos);
+	led_off();
 
 	adc_init();
 	adc_start();
@@ -71,18 +83,26 @@ void entry(void)
 	/* run advertisement in background every 995ms */
 
 	if (nrf_gpio_pin_read(CONFIG_TEST_PIN)) {
-		pin_set(CONFIG_LED_PIN);
+		led_on();
 		timer_wait_ms(500);
-		pin_clear(CONFIG_LED_PIN);
+		led_off();
 		radio_interval_ms(995);
+		g_debug = true;
 	} else {
-		pin_set(CONFIG_LED_PIN);
+		led_on();
 		timer_wait_ms(50);
-		pin_clear(CONFIG_LED_PIN);
+		led_off();
 		radio_interval_ms(9995);
+		g_debug = false;
 	}
 
 	/* infinite foreground loop */
-	while(TRUE)
-		__WFE();
+	while(TRUE) {
+		led_on();
+		timer_wait_ms(200);
+		adc_start();
+		temp_start();
+		led_off();
+		timer_wait_ms(g_debug ? 1500 : 10000);
+	}
 }
